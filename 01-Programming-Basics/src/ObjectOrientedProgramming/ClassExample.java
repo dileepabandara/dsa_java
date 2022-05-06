12cfc7349843619fbb42b34296ffb9bc9293224b
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-05-06 19:03:18 +0530

    Update ClassExample.java

--- 01-Programming-Basics/src/ObjectOrientedProgramming/ClassExample.java
+++ 01-Programming-Basics/src/ObjectOrientedProgramming/ClassExample.java
@@ -2,8 +2,13 @@ package ObjectOrientedProgramming;
 
 public class ClassExample {
 
+    static String name = "sample";
+    static int id = 1;
+    static double value = 20.25;
+
     public static void main(String[] args) {
         System.out.println("This is a sample class");
+        System.out.println("Name: " + name + " | id: " + id + " | Value: " + value);
     }
 
 }
