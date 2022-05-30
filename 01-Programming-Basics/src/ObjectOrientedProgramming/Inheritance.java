1de5b7d26b4bc8cf87cdefe75c9511b8cdc6fd5a
Dileepa Bandara <80202913+dileepabandara@users.noreply.github.com>
2022-05-30 13:49:59 +0530

    Update Inheritance

--- 01-Programming-Basics/src/ObjectOrientedProgramming/Inheritance.java
+++ 01-Programming-Basics/src/ObjectOrientedProgramming/Inheritance.java
@@ -1,25 +1,29 @@
 package ObjectOrientedProgramming;
 
+class SuperClass {
+
+    String className = "SuperClass";
+    private int id = 1;
+
+    void workMode() {
+        System.out.println("SuperClass Work Mode!");
+    }
+
+}
+
+class SubClass extends SuperClass {
+    String version = "1.0.1";
+}
+
+
 public class Inheritance {
-}
 
-
-class OldPhone {
-
-    static String brandName = "OLD";
-    String colors = "BLACK";
-
-    void call() {
-    }
-
-    void sms() {
-    }
-
-}
-
-class NewPhone extends OldPhone {
+    static SubClass subClass = new SubClass();
 
     public static void main(String[] args) {
-        System.out.println(brandName);
+        System.out.println("Class Name: " + subClass.className);
+        // System.out.println("Class ID: " + subClass.id);
+        System.out.println("Class Version: " + subClass.version);
+        subClass.workMode();
     }
 }
--- 01-Programming-Basics/src/ObjectOrientedProgramming/README.md
+++ 01-Programming-Basics/src/ObjectOrientedProgramming/README.md
@@ -5,4 +5,5 @@
 01 - Basic Standards (BasicStandards.java)  
 02 - Classes and Objects (ClassesAndObjects.java)  
 03 - Constructor (Constructor.java)  
-04 - MethodOverloading (MethodOverloading.java)
+04 - MethodOverloading (MethodOverloading.java)  
+05 - Inheritance (Inheritance.java)
